32b9c2f0a58fef697de2fb817cd8944f3de9e30b
Oleg Hyrbu <codercveu@gmail.com>
2019-09-25 16:25:08 +0300

    Add promt to chmodown.sh

--- chmodown.sh
+++ chmodown.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
-echo 'chown codercv:www-data '$1
-sudo chown -R codercv:www-data $1
-echo 'chmod directories 775'
+read -r -p "Chose action for $1 in
+[M/m] - chmod for all dirs to 775 and for files 664
+[O/o] - chown $USER:www-data
+[Y/y] - both chmod + chown
+[N/n] - Cancel
+" input
+case $input in
+    [yY])
+echo "chown $USER:www-data "$1
+sudo chown -R $USER:www-data $1
+echo "chmod directories 775"
 sudo find $1/. -type d -exec chmod 775 {} \;
-echo 'chmod files 664'
+echo "chmod files 664"
 sudo find $1/. -type f -exec chmod 664 {} \;
-echo "chown/chmod - DONE"
-
+echo "chown and chmod - DONE for $1"
+ ;;
+    [mM])
+ echo "chmod directories 775"
+ sudo find $1/. -type d -exec chmod 775 {} \;
+ echo "chmod files 664"
+ sudo find $1/. -type f -exec chmod 664 {} \;
+ echo "chmod - DONE for $1"
+ ;;
+    [oO])
+ echo "chown $USER:www-data "$1
+ sudo chown -R $USER:www-data $1
+ echo "chown - DONE for $1"
+ ;;
+    [nN])
+ echo "Operation Canceled. Permissions and owners don't changed for $1"
+ ;;
+    *)
+ echo "Invalid input..."
+ ;;
+esac
